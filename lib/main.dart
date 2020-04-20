b35b2ae062ab9afd3f2027a660276a9595d75e75
praveen <ptest5557@gmail.com>
2020-04-20 23:07:52 +0530

    part -2

--- lib/main.dart
+++ lib/main.dart
@@ -9,30 +9,83 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      backgroundColor: Colors.deepOrange.shade300,
-      appBar: AppBar(backgroundColor: Colors.deepOrangeAccent),
-      body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            Container(),
-            CircleAvatar(
-              radius: 50.0,
-              backgroundImage: AssetImage('images/p.png'),
-            ),
-            Text('praveen dev',
-                style: TextStyle(
-                  fontFamily: 'Pacifico',
-                  fontSize: 50.0,
-                )),
-            Text('Flutter Mobile App Developer',
+      home: Scaffold(
+        backgroundColor: Colors.greenAccent,
+        appBar: AppBar(backgroundColor: Colors.red),
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/p.png'),
+              ),
+              Text(
+                'Praveen',
                 style: TextStyle(
                   fontFamily: 'Pacifico',
                   fontSize: 20.0,
-                ))
-          ],
+                ),
+              ),
+              Text(
+                'Flutter Developer',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 29.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.green,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '+91 9901322153',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 29.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.green,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'praveendb009@gmail.com',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
-    ));
+    );
   }
 }
